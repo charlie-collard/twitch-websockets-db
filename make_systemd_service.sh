17afdab5aa32f50a98523a0e2394c65599fc7113
Charlie Collard <charliecollard0@gmail.com>
2022-07-03 22:11:19 +0100

    Add systemd service

---
+++ make_systemd_service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat <<- 'EOF' > /etc/systemd/system/twitch-websockets-db.service
+[Unit]
+Description="Starts up twitch websocket listener"
+
+[Service]
+User=charlie
+WorkingDirectory=/home/charlie/workspace/twitch-websockets-db
+ExecStart=bash -c "source credentials.sh && python3.9 ws.py"
+Restart=always
+StandardOutput=journal
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable twitch-websockets-db.service
+systemctl restart twitch-websockets-db.service
