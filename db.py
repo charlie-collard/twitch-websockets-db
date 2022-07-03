0437e27d5009ba3b475cfa373acab08e56f448ae
Charlie Collard <charliecollard0@gmail.com>
2022-07-03 18:42:13 +0100

    Receive and store prediction changes

--- db.py
+++ db.py
@@ -1,19 +1,6 @@
 import sqlite3
 from datetime import datetime
 
-{
-  "channel_id": "31758516",
-  "type": "broadcast_settings_update",
-  "channel": "king20333",
-  "old_status": ".",
-  "status": ".",
-  "old_game": "IRL",
-  "game": "Games + Demos",
-  "old_game_id": 494717,
-  "game_id": 66082
-}
-
-
 TABLES = [
     """
     create table if not exists broadcast_settings_messages (
@@ -40,21 +27,22 @@ TABLES = [
         eventStatus text not null,
         eventTitle text not null,
         eventPredictionWindowSeconds integer not null,
-        eventCreatedAt datetime not null,
+        eventCreatedAtUtc datetime not null,
         eventCreatedByType text not null,
         eventCreatedByUserTwitchID text not null,
         eventCreatedByUserDisplayName text not null,
-        eventEndedAt datetime,
+        eventEndedAtUtc datetime,
         eventEndedByType text,
         eventEndedByUserTwitchID text,
         eventEndedByUserDisplayName text,
-        eventLockedAt datetime,
+        eventLockedAtUtc datetime,
         eventLockedByType text,
         eventLockedByUserTwitchID text,
         eventLockedByUserDisplayName text,
         eventWinningOutcomeTwitchId text,
 
         outcome0TwitchID text not null,
+        outcome0Title text not null,
         outcome0Color text not null,
         outcome0TotalPoints integer not null,
         outcome0TotalUsers integer not null,
@@ -62,70 +50,237 @@ TABLES = [
         outcome0BadgeSetTwitchID text not null,
 
         outcome1TwitchID text not null,
+        outcome1Title text not null,
         outcome1Color text not null,
         outcome1TotalPoints integer not null,
         outcome1TotalUsers integer not null,
         outcome1BadgeVersion text not null,
         outcome1BadgeSetTwitchID text not null,
 
-        outcome2TwitchID text not null,
-        outcome2Color text not null,
-        outcome2TotalPoints integer not null,
-        outcome2TotalUsers integer not null,
-        outcome2BadgeVersion text not null,
-        outcome2BadgeSetTwitchID text not null,
+        outcome2TwitchID text,
+        outcome2Title text,
+        outcome2Color text,
+        outcome2TotalPoints integer,
+        outcome2TotalUsers integer,
+        outcome2BadgeVersion text,
+        outcome2BadgeSetTwitchID text,
 
-        outcome3TwitchID text not null,
-        outcome3Color text not null,
-        outcome3TotalPoints integer not null,
-        outcome3TotalUsers integer not null,
-        outcome3BadgeVersion text not null,
-        outcome3BadgeSetTwitchID text not null,
+        outcome3TwitchID text,
+        outcome3Title text,
+        outcome3Color text,
+        outcome3TotalPoints integer,
+        outcome3TotalUsers integer,
+        outcome3BadgeVersion text,
+        outcome3BadgeSetTwitchID text,
 
-        outcome4TwitchID text not null,
-        outcome4Color text not null,
-        outcome4TotalPoints integer not null,
-        outcome4TotalUsers integer not null,
-        outcome4BadgeVersion text not null,
-        outcome4BadgeSetTwitchID text not null,
+        outcome4TwitchID text,
+        outcome4Title text,
+        outcome4Color text,
+        outcome4TotalPoints integer,
+        outcome4TotalUsers integer,
+        outcome4BadgeVersion text,
+        outcome4BadgeSetTwitchID text,
 
-        outcome5TwitchID text not null,
-        outcome5Color text not null,
-        outcome5TotalPoints integer not null,
-        outcome5TotalUsers integer not null,
-        outcome5BadgeVersion text not null,
-        outcome5BadgeSetTwitchID text not null,
+        outcome5TwitchID text,
+        outcome5Title text,
+        outcome5Color text,
+        outcome5TotalPoints integer,
+        outcome5TotalUsers integer,
+        outcome5BadgeVersion text,
+        outcome5BadgeSetTwitchID text,
 
-        outcome6TwitchID text not null,
-        outcome6Color text not null,
-        outcome6TotalPoints integer not null,
-        outcome6TotalUsers integer not null,
-        outcome6BadgeVersion text not null,
-        outcome6BadgeSetTwitchID text not null,
+        outcome6TwitchID text,
+        outcome6Title text,
+        outcome6Color text,
+        outcome6TotalPoints integer,
+        outcome6TotalUsers integer,
+        outcome6BadgeVersion text,
+        outcome6BadgeSetTwitchID text,
 
-        outcome7TwitchID text not null,
-        outcome7Color text not null,
-        outcome7TotalPoints integer not null,
-        outcome7TotalUsers integer not null,
-        outcome7BadgeVersion text not null,
-        outcome7BadgeSetTwitchID text not null,
+        outcome7TwitchID text,
+        outcome7Title text,
+        outcome7Color text,
+        outcome7TotalPoints integer,
+        outcome7TotalUsers integer,
+        outcome7BadgeVersion text,
+        outcome7BadgeSetTwitchID text,
 
-        outcome8TwitchID text not null,
-        outcome8Color text not null,
-        outcome8TotalPoints integer not null,
-        outcome8TotalUsers integer not null,
-        outcome8BadgeVersion text not null,
-        outcome8BadgeSetTwitchID text not null,
+        outcome8TwitchID text,
+        outcome8Title text,
+        outcome8Color text,
+        outcome8TotalPoints integer,
+        outcome8TotalUsers integer,
+        outcome8BadgeVersion text,
+        outcome8BadgeSetTwitchID text,
 
-        outcome9TwitchID text not null,
-        outcome9Color text not null,
-        outcome9TotalPoints integer not null,
-        outcome9TotalUsers integer not null,
-        outcome9BadgeVersion text not null,
-        outcome9BadgeSetTwitchID text not null
+        outcome9TwitchID text,
+        outcome9Title text,
+        outcome9Color text,
+        outcome9TotalPoints integer,
+        outcome9TotalUsers integer,
+        outcome9BadgeVersion text,
+        outcome9BadgeSetTwitchID text
     );
     """
 ]
+
+def to_timestamp(timestamp):
+    i = timestamp.find(".")
+    j = timestamp.find("Z")
+    if i == -1:
+        return datetime.fromisoformat(timestamp[:j])
+
+    fractional = float(timestamp[i:j])
+    formatted = timestamp[:i] + f'{fractional:.6f}'[1:]
+    return datetime.fromisoformat(formatted)
+
+
+def get_nullable(event_data, key, post_process=lambda x: x):
+    return post_process(event_data[key]) if event_data.get(key) else None
+
+
+def insert_prediction_message(cursor, data):
+    event_data = data["data"]["event"]
+    to_insert = {
+        "utcTimestamp": to_timestamp(data["data"]["timestamp"]),
+        "channelTwitchID": event_data["channel_id"],
+        "eventTwitchID": event_data["id"],
+        "eventType": data["type"],
+        "eventStatus": event_data["status"],
+        "eventTitle": event_data["title"],
+        "eventPredictionWindowSeconds": event_data["prediction_window_seconds"],
+        "eventCreatedAtUtc": to_timestamp(event_data["created_at"]),
+        "eventCreatedByType": event_data["created_by"]["type"],
+        "eventCreatedByUserTwitchID": event_data["created_by"]["user_id"],
+        "eventCreatedByUserDisplayName": event_data["created_by"]["user_display_name"],
+        "eventEndedAtUtc": get_nullable(event_data, "ended_at", to_timestamp),
+        "eventEndedByType": get_nullable(event_data, "ended_by", lambda x: x["type"]),
+        "eventEndedByUserTwitchID": get_nullable(event_data, "ended_by", lambda x: x["user_id"]),
+        "eventEndedByUserDisplayName": get_nullable(event_data, "ended_by", lambda x: x["user_display_name"]),
+        "eventLockedAtUtc": get_nullable(event_data, "locked_at", to_timestamp),
+        "eventLockedByType": get_nullable(event_data, "locked_by", lambda x: x["type"]),
+        "eventLockedByUserTwitchID": get_nullable(event_data, "locked_by", lambda x: x["user_id"]),
+        "eventLockedByUserDisplayName": get_nullable(event_data, "locked_by", lambda x: x["user_display_name"]),
+        "eventWinningOutcomeTwitchId": get_nullable(event_data, "winning_outcome_id"),
+    }
+
+    for i in range(10):
+        outcome = event_data["outcomes"][i] if len(event_data["outcomes"]) > i else None
+        to_insert |= {
+            f"outcome{i}TwitchID": outcome["id"],
+            f"outcome{i}Title": outcome["id"],
+            f"outcome{i}Color": outcome["color"],
+            f"outcome{i}TotalPoints": outcome["total_points"],
+            f"outcome{i}TotalUsers": outcome["total_users"],
+            f"outcome{i}BadgeVersion": outcome["badge"]["version"],
+            f"outcome{i}BadgeSetTwitchID": outcome["badge"]["set_id"],
+        } if outcome else {
+            f"outcome{i}TwitchID": None,
+            f"outcome{i}Title": None,
+            f"outcome{i}Color": None,
+            f"outcome{i}TotalPoints": None,
+            f"outcome{i}TotalUsers": None,
+            f"outcome{i}BadgeVersion": None,
+            f"outcome{i}BadgeSetTwitchID": None,
+        }
+
+
+    cursor.execute("""
+    insert into prediction_messages (
+        utcTimestamp,
+        channelTwitchID,
+        eventTwitchID,
+        eventType,
+        eventStatus,
+        eventTitle,
+        eventPredictionWindowSeconds,
+        eventCreatedAtUtc,
+        eventCreatedByType,
+        eventCreatedByUserTwitchID,
+        eventCreatedByUserDisplayName,
+        eventEndedAtUtc,
+        eventEndedByType,
+        eventEndedByUserTwitchID,
+        eventEndedByUserDisplayName,
+        eventLockedAtUtc,
+        eventLockedByType,
+        eventLockedByUserTwitchID,
+        eventLockedByUserDisplayName,
+        eventWinningOutcomeTwitchId,
+        outcome0TwitchID,
+        outcome0Title,
+        outcome0Color,
+        outcome0TotalPoints,
+        outcome0TotalUsers,
+        outcome0BadgeVersion,
+        outcome0BadgeSetTwitchID,
+        outcome1TwitchID,
+        outcome1Title,
+        outcome1Color,
+        outcome1TotalPoints,
+        outcome1TotalUsers,
+        outcome1BadgeVersion,
+        outcome1BadgeSetTwitchID,
+        outcome2TwitchID,
+        outcome2Title,
+        outcome2Color,
+        outcome2TotalPoints,
+        outcome2TotalUsers,
+        outcome2BadgeVersion,
+        outcome2BadgeSetTwitchID,
+        outcome3TwitchID,
+        outcome3Title,
+        outcome3Color,
+        outcome3TotalPoints,
+        outcome3TotalUsers,
+        outcome3BadgeVersion,
+        outcome3BadgeSetTwitchID,
+        outcome4TwitchID,
+        outcome4Title,
+        outcome4Color,
+        outcome4TotalPoints,
+        outcome4TotalUsers,
+        outcome4BadgeVersion,
+        outcome4BadgeSetTwitchID,
+        outcome5TwitchID,
+        outcome5Title,
+        outcome5Color,
+        outcome5TotalPoints,
+        outcome5TotalUsers,
+        outcome5BadgeVersion,
+        outcome5BadgeSetTwitchID,
+        outcome6TwitchID,
+        outcome6Title,
+        outcome6Color,
+        outcome6TotalPoints,
+        outcome6TotalUsers,
+        outcome6BadgeVersion,
+        outcome6BadgeSetTwitchID,
+        outcome7TwitchID,
+        outcome7Title,
+        outcome7Color,
+        outcome7TotalPoints,
+        outcome7TotalUsers,
+        outcome7BadgeVersion,
+        outcome7BadgeSetTwitchID,
+        outcome8TwitchID,
+        outcome8Title,
+        outcome8Color,
+        outcome8TotalPoints,
+        outcome8TotalUsers,
+        outcome8BadgeVersion,
+        outcome8BadgeSetTwitchID,
+        outcome9TwitchID,
+        outcome9Title,
+        outcome9Color,
+        outcome9TotalPoints,
+        outcome9TotalUsers,
+        outcome9BadgeVersion,
+        outcome9BadgeSetTwitchID
+    ) values (
+    """ + ",\n".join(f":{key}" for key, value in to_insert.items()) + ");"
+    , to_insert)
+    cursor.execute("commit;")
 
 def insert_broadcast_settings_message(cursor, data):
     to_insert = {
@@ -152,17 +307,8 @@ def insert_broadcast_settings_message(cursor, data):
         oldGameTwitchID,
         gameTwitchID
     ) values (
-        :channelTwitchID,
-        :utcTimestamp,
-        :channelName,
-        :oldStatus,
-        :status,
-        :oldGame,
-        :game,
-        :oldGameTwitchID,
-        :gameTwitchID
-    )
-    """, to_insert)
+    """ + ",\n".join(f":{key}" for key, value in to_insert.items()) + ");"
+    , to_insert)
     cursor.execute("commit;")
 
 
--- ws.py
+++ ws.py
@@ -5,13 +5,18 @@ import requests
 import os
 import sqlite3
 
-from db import create_tables, insert_broadcast_settings_message
+from db import create_tables, insert_broadcast_settings_message, insert_prediction_message
 
 KING20333_ID = "31758516"
 GBP_ID = "53831996"
+HIKARU_ID = "103268673"
+CR1T_ID = "132230344"
+BAHROO_ID = "40972890"
 
-PREDICTIONS_TOPIC = f"predictions-channel-v1.{KING20333_ID}"
-GAME_CHANGE_TOPIC = f"broadcast-settings-update.{KING20333_ID}"
+LISTEN_ID = BAHROO_ID
+
+PREDICTIONS_TOPIC = f"predictions-channel-v1.{LISTEN_ID}"
+GAME_CHANGE_TOPIC = f"broadcast-settings-update.{LISTEN_ID}"
 
 test_broadcast_settings_message = {
   "channel_id": "31758516",
@@ -23,6 +28,94 @@ test_broadcast_settings_message = {
   "game": "Games + Demos",
   "old_game_id": 494717,
   "game_id": 66082
+}
+
+test_predictions_message = {
+  "type": "event-created",
+  "data": {
+    "timestamp": "2022-07-01T21:49:44.297583928Z",
+    "event": {
+      "id": "1f9292d3-de06-4cb0-85a9-e179422cf1d7",
+      "channel_id": "53831996",
+      "created_at": "2022-07-01T21:49:44.280353624Z",
+      "created_by": {
+        "type": "USER",
+        "user_id": "41677803",
+        "user_display_name": "Jullicent",
+        "extension_client_id": None
+      },
+      "ended_at": None,
+      "ended_by": None,
+      "locked_at": None,
+      "locked_by": None,
+      "outcomes": [
+        {
+          "id": "da8948e9-38ff-4e78-a9be-747feca971da",
+          "color": "BLUE",
+          "title": "All 4",
+          "total_points": 0,
+          "total_users": 0,
+          "top_predictors": [],
+          "badge": {
+            "version": "blue-1",
+            "set_id": "predictions"
+          }
+        },
+        {
+          "id": "a958f32c-748b-4ced-81e2-70c3f04f2e3d",
+          "color": "BLUE",
+          "title": "3 of them",
+          "total_points": 0,
+          "total_users": 0,
+          "top_predictors": [],
+          "badge": {
+            "version": "blue-2",
+            "set_id": "predictions"
+          }
+        },
+        {
+          "id": "6b931ce5-7e09-4da4-8f39-8ccedaad660e",
+          "color": "BLUE",
+          "title": "2 of them",
+          "total_points": 0,
+          "total_users": 0,
+          "top_predictors": [],
+          "badge": {
+            "version": "blue-3",
+            "set_id": "predictions"
+          }
+        },
+        {
+          "id": "29b9f3ec-36ad-4ddd-9685-ebf8bc3d8c1a",
+          "color": "BLUE",
+          "title": "Only 1",
+          "total_points": 0,
+          "total_users": 0,
+          "top_predictors": [],
+          "badge": {
+            "version": "blue-4",
+            "set_id": "predictions"
+          }
+        },
+        {
+          "id": "0587e9ee-ef8b-4e4d-b4df-5621eb0aaea4",
+          "color": "BLUE",
+          "title": "None :( ",
+          "total_points": 0,
+          "total_users": 0,
+          "top_predictors": [],
+          "badge": {
+            "version": "blue-5",
+            "set_id": "predictions"
+          }
+        }
+      ],
+      "prediction_window_seconds": 120,
+      "status": "ACTIVE",
+      "title": "How much of the squad will make the finals?",
+      "winning_outcome_id": None
+    }
+  }
 }
 
 
@@ -51,18 +144,13 @@ def spawn_ping_task(websocket):
     ping_task.add_done_callback(background_tasks.discard)
 
 
-def handle_prediction_message(data):
-    print("Received prediction message")
-    pass
-
-
 def handle_message(message, cursor):
     print(message)
     message = json.loads(message)
     if message["type"] == "MESSAGE":
         topic, data = message["data"]["topic"], json.loads(message["data"]["message"])
         if topic == PREDICTIONS_TOPIC:
-            handle_prediction_message(data)
+            insert_prediction_message(cursor, data)
         if topic == GAME_CHANGE_TOPIC:
             insert_broadcast_settings_message(cursor, data)
 
@@ -93,4 +181,5 @@ async def run(cursor):
 with sqlite3.connect("websockets.db") as connection:
     cursor = connection.cursor()
     create_tables(cursor)
+
     asyncio.run(run(cursor))
