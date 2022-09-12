7975e2ef1019e00a598a0342aea52668c96d995b
Charlie Collard <charliecollard0@gmail.com>
2022-09-12 16:17:06 +0100

    Factor out calculating odds logic so that it can be done automatically

--- calculate_odds.py
+++ calculate_odds.py
@@ -2,43 +2,55 @@ import pandas as pd
 import sqlite3
 import csv
 
-channel_id = "14371185"
-game_name = "Rumbleverse"
-event_title = "we finish"
 
-with sqlite3.connect('websockets.db') as connection:
-    cursor = connection.cursor()
-    query = cursor.execute(f"select * from prediction_messages where channelTwitchID = '{channel_id}' and eventStatus = 'RESOLVED';")
-    cols = [column[0] for column in query.description]
-    resolved_bets = pd.DataFrame(cursor.fetchall(), columns=cols)
-    query = cursor.execute(f"select utcTimestamp, game from broadcast_settings_messages where channelTwitchID = '{channel_id}';")
-    cols = [column[0] for column in query.description]
-    games = pd.DataFrame(cursor.fetchall(), columns=cols)
+def load_resolved_bets(channel_id):
+    with sqlite3.connect('websockets.db') as connection:
+        cursor = connection.cursor()
+        query = cursor.execute(f"select * from prediction_messages where channelTwitchID = '{channel_id}' and eventStatus = 'RESOLVED';")
+        cols = [column[0] for column in query.description]
+        resolved_bets = pd.DataFrame(cursor.fetchall(), columns=cols)
+    return resolved_bets
 
 
-def timestamp_to_game(timestamp):
-    return games[games["utcTimestamp"] < timestamp].iloc[-1]["game"]
+def load_game_history(channel_id):
+    with sqlite3.connect('websockets.db') as connection:
+        cursor = connection.cursor()
+        query = cursor.execute(f"select utcTimestamp, game from broadcast_settings_messages where channelTwitchID = '{channel_id}';")
+        cols = [column[0] for column in query.description]
+        games = pd.DataFrame(cursor.fetchall(), columns=cols)
+    return games
 
-id_columns = resolved_bets.columns[resolved_bets.columns.str.contains("outcome\d+TwitchID", regex=True)]
-def get_winning_outcome_index(row):
-    return 1 + int(id_columns[row[id_columns] == row["eventWinningOutcomeTwitchId"]].str.extract("outcome(\d+)TwitchID")[0][0])
+
+def timestamp_to_game(games):
+    return lambda timestamp: games[games["utcTimestamp"] < timestamp].iloc[-1]["game"]
 
 
-out = pd.DataFrame()
-out["created_at"] = resolved_bets["eventCreatedAtUtc"]
-out["game_name"] = resolved_bets["eventCreatedAtUtc"].apply(timestamp_to_game)
-out["title"] = resolved_bets["eventTitle"]
-out["window_seconds"] = resolved_bets["eventPredictionWindowSeconds"]
-out["win_index"] = resolved_bets.apply(get_winning_outcome_index, axis=1)
-for i in range(10):
-    out[f"title_{i+1}"] = resolved_bets[f"outcome{i}Title"]
-    out[f"points_{i+1}"] = resolved_bets[f"outcome{i}TotalPoints"]
-    out[f"users_{i+1}"] = resolved_bets[f"outcome{i}TotalUsers"]
+def get_winning_outcome_index(resolved_bets):
+    id_columns = resolved_bets.columns[resolved_bets.columns.str.contains("outcome\d+TwitchID", regex=True)]
+    return lambda row: 1 + int(id_columns[row[id_columns] == row["eventWinningOutcomeTwitchId"]].str.extract("outcome(\d+)TwitchID")[0][0])
 
-out = pd.concat([pd.read_csv("NorthernLion Predictions - predictions.csv"), out])
-out = out[out["game_name"] == game_name] if game_name else out
-out = out[out["title"].str.contains(event_title)] if event_title else out
-out = out[out["created_at"] > "2022-09-01"]
 
-assert len(set(tuple(row[f"title_{j+1}"] for j in range(10)) for i, row in out.iterrows())) == 1
-print(out.win_index.value_counts(normalize=True).sort_index())
+def calculate_odds(channel_id, game_name, event_title, outcome_titles):
+    resolved_bets, games = load_resolved_bets(channel_id), load_game_history(channel_id)
+
+    out = pd.DataFrame()
+    out["created_at"] = resolved_bets["eventCreatedAtUtc"]
+    out["game_name"] = resolved_bets["eventCreatedAtUtc"].apply(timestamp_to_game(games))
+    out["title"] = resolved_bets["eventTitle"]
+    out["window_seconds"] = resolved_bets["eventPredictionWindowSeconds"]
+    out["win_index"] = resolved_bets.apply(get_winning_outcome_index(resolved_bets), axis=1)
+    for i in range(10):
+        out[f"title_{i+1}"] = resolved_bets[f"outcome{i}Title"]
+        out[f"points_{i+1}"] = resolved_bets[f"outcome{i}TotalPoints"]
+        out[f"users_{i+1}"] = resolved_bets[f"outcome{i}TotalUsers"]
+
+    out = pd.concat([pd.read_csv("NorthernLion Predictions - predictions.csv"), out])
+    out = out[out["game_name"] == game_name]
+    out = out[out["title"] == event_title]
+    for i in range(10):
+        outcome_title = outcome_titles[i]
+        out = out[out[f"title_{i+1}"] == outcome_title] if outcome_title is not None else out[out[f"title_{i+1}"].isnull()]
+
+    odds = out.win_index.value_counts(normalize=True).sort_index()
+    odds.index = [title for title in outcome_titles if title is not None]
+    return len(out), odds
--- live_odds.py
+++ live_odds.py
@@ -1,24 +1,34 @@
+from calculate_odds import calculate_odds, load_game_history, timestamp_to_game
 from ws import twitch_websocket_runner, NL_ID
+from datetime import datetime
 
 import asyncio
 
 CHANNELS = [NL_ID]
 TOPICS = ["predictions-channel-v1"]
 
+
+odds = None
 def message_handler(message, topic):
-    if "predictions-channel-v1" in topic and message['status'] in ['ACTIVE', 'LOCKED']:
-        odds = pd.Series([
-            0.333333,
-            0.252874,
-            0.356322,
-            0.057471,
-        ])
-        # Try to predict last-minute betting + my bet
-        extra = 60000 if message['status'] == 'ACTIVE' else 0
-        points = pd.Series(map(lambda x: int(x['total_points']) + extra, message['outcomes']))
-        payouts = points.sum() / points
-        odds.index = ['Outside of top 20', '20-11', '10-2', '1']
-        payouts.index = ['Outside of top 20', '20-11', '10-2', '1']
-        print((odds * payouts).sort_values())
+    global odds
+    event_data = message["data"]["event"]
+    if event_data["status"] in ['ACTIVE', 'LOCKED']:
+        if message["type"] == "event-created":
+            channel_id = event_data["channel_id"]
+            assert channel_id in CHANNELS
+            game_name = timestamp_to_game(load_game_history(channel_id))(datetime.now().isoformat())
+            event_title = event_data["title"]
+            outcomes = event_data["outcomes"]
+            outcome_titles = [outcome["title"] for outcome in outcomes] + [None] * (10 - len(outcomes))
+            samples, odds = calculate_odds(channel_id, game_name, event_title, outcome_titles)
+            print(f"Found odds for game {game_name}, event title {event_title}, with {samples} samples:", odds)
+
+        if odds:
+            # Try to predict last-second betting + my bet
+            extra = 60000 if message['status'] == 'ACTIVE' else 0
+            points = pd.Series(map(lambda x: int(x['total_points']) + extra, message['outcomes']))
+            payouts = points.sum() / points
+            payouts.index = [title for title in outcome_titles if title is not None]
+            print((odds * payouts).sort_values())
 
 asyncio.run(twitch_websocket_runner(CHANNELS, TOPICS, message_handler))
